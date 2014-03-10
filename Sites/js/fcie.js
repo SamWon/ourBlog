145bdb6ed240ba9ba18c4c931a8b42d944664347
SamWon <285505854@qq.com>
2014-03-10 20:52:11 +0800

    fcie

---
+++ Sites/js/fcie.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("body").html(
+        '<p>为了更好的体验，请使用更高级的浏览器：</p>' +
+        '<span>下载地址：</span>' +
+        '<a href="http://windows.microsoft.com/en-HK/internet-explorer/download-ie">ie9</a>' +
+        ' <a href="http://www.mozilla.org/zh-CN/firefox/new/">firefox</a>' +
+        ' <a href="https://www.google.com/intl/en/chrome/browser/">chrome</a>'
+    );
+});
